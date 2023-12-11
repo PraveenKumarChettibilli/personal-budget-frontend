e935159389b297b0f8fd453f3b3fae7f6e276b56
PraveenKumarChettibilli <pchettib@uncc.edu>
2023-12-11 17:41:55 -0500

    update package

--- src/app/authguard.guard.ts
+++ src/app/authguard.guard.ts
@@ -1,12 +1,12 @@
 import { Route } from '@angular/compiler/src/core';
 import { Injectable } from '@angular/core';
-import { CanActivate, Data, Router} from '@angular/router';
+import { Data, Router } from '@angular/router';
 import { DataService } from './data.service';
 
 @Injectable({
   providedIn: 'root'
 })
-export class AuthguardGuard implements CanActivate {
+export class AuthguardGuard  {
   constructor(private _dataservice:DataService,private router:Router){
 
   }
